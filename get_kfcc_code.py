dad3f7859d11d2cd09cccd0de6da9a09c223ab32
cyberjam <jamsince1994@naver.com>
2023-06-14 13:24:57 +0900

    REFACTOR: 지점목록 페이지 반환 기능 분리

--- get_kfcc_code.py
+++ get_kfcc_code.py
@@ -1,5 +1,6 @@
 from typing import Final, Dict
 import requests
+from bs4 import BeautifulSoup
 
 REGION: Final = ('서울', '인천', '경기', '강원', '충남', '충북', '대전', '경북',
                  '경남', '대구', '부산', '울산', '전북', '전남', '광주', '제주', '세종')
@@ -11,12 +12,24 @@ headers: Dict = {
 }
 
 
+def get_page_source(region_name):
+    """지역별 지점 목록을 나타내는 페이지 소스코드를 반환합니다.
+
+    Args:
+        region_name (string): 대한민국 광역, 자치, 도 단위 지역
+
+    Returns:
+        string: 해당 지역 지점 목록 html이 text로 반환
+    """
+    params = {'r1': region_name, 'r2': ''}
+    request_object = requests.get(URL, params=params, headers=headers)
+    return request_object.text
+
+
 def main():
     """main 실행함수"""
     for region_name in REGION:
-        params = {'r1': region_name, 'r2': ''}
-        request_object = requests.get(URL, params=params, headers=headers)
-        print(request_object.text)
+        get_page_source(region_name)
 
 
 if __name__ == '__main__':
