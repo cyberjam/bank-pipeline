a857b1a9df39012d4279f0d0eb7f6e55ffc8a5c2
cyberjam <jamsince1994@naver.com>
2023-06-24 00:56:48 +0900

    FEAT: 지역 반환 및 데이터 숫자화

--- get_kfcc_indicator.py
+++ get_kfcc_indicator.py
@@ -10,7 +10,7 @@ from utils import scraper
 def load_bank_code_info():
     with open(GLOBAL_CONSTANT['CODE_JSON_PATH'], encoding='UTF-8') as file:
         bank_infos = json.load(file)
-        return {bank_info['gmgoCd']: bank_info['name'] for bank_info in bank_infos}
+        return {bank_info['gmgoCd']: (bank_info['name'], bank_info['r1']) for bank_info in bank_infos}
 
 
 def build_payload(bank_code):
@@ -46,20 +46,22 @@ def extract_indicator(processed_raw_data, address):
     return processed_raw_data.get(address, f'{" "*3}')[2]
 
 
-def build_indicator_data(processed_raw_data, bank_code, bank_name):
+def build_indicator_data(processed_raw_data, bank_code, bank_name, province):
     return {
+        "행정구역": province,
         "지점명": bank_name,
         "지점코드": bank_code,
-        "위험가중자산대비 자기자본비율": extract_indicator(processed_raw_data, '25000001'),
-        "순고정이하 여신비율": extract_indicator(processed_raw_data, '25000005'),
-        "유동성 비율": extract_indicator(processed_raw_data, '25000007'),
-        "총자산 순이익률": extract_indicator(processed_raw_data, '25000009'),
-        "경영실태 평가": extract_indicator(processed_raw_data, '31000001'),
+        "위험가중자산대비 자기자본비율": float(extract_indicator(processed_raw_data, '25000001').replace(",", "")),
+        "순고정이하 여신비율": float(extract_indicator(processed_raw_data, '25000005').replace(",", "")),
+        "유동성 비율": float(extract_indicator(processed_raw_data, '25000007').replace(",", "")),
+        "총자산 순이익률": float(extract_indicator(processed_raw_data, '25000009').replace(",", "")),
+        "경영실태 평가": int(re.search("[1-5]", extract_indicator(processed_raw_data, '31000001')).group()),
     }
 
 
 def fetch_bank_indicators():
-    for bank_code, bank_name in tqdm(load_bank_code_info().items()):
+    for bank_code, bank_info in tqdm(load_bank_code_info().items()):
+        bank_name, province = bank_info
         payload = build_payload(bank_code)
         html = scraper.fetch_page_source(method='POST',
                                          url=BANK_INDICATOR_CONSTANT['URL'],
@@ -68,7 +70,7 @@ def fetch_bank_indicators():
         soup = scraper.create_soup(html)
         raw_data = extract_raw_data(soup)
         parsed_raw_data = parse_raw_data(raw_data)
-        yield build_indicator_data(parsed_raw_data, bank_code, bank_name)
+        yield build_indicator_data(parsed_raw_data, bank_code, bank_name, province)
 
 
 def write_sheet(bank_indicator):
